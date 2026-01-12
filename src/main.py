b35fb15f80d477856c0ef55f69385104ec864c60
Krystian Kolodziejczyk <krystiankolodziejczyk2004@gmail.com>
2026-01-12 11:03:17 +0100

    Finish script that works

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 import os
 
-
+app: list[str] = ["modules", "shared", "core"]
 domain: list[str] = ["models", "enums", "repositories", "services"]
 infrastructure: list[str] = ["repositories", "services"]
 presentation: list[str] = ["controllers", "dto", "responses"]
 
 
 def make_dir(dirName: str, dirNameSpace: list[str]) -> None:
-    os.system(f"mkdir {dirName}")
+    os.system(f"mkdir {dirName} && touch {dirName}/__init__.py")
     os.chdir(dirName)
     for oneDirName in dirNameSpace:
-        os.system(f"mkdir {oneDirName}")
+        os.system(f"mkdir {oneDirName} && touch {oneDirName}/__init__.py")
     os.chdir("..")
 
 
@@ -34,10 +34,11 @@ def create_module(name: str) -> None:
 # create_module(name="program")
 
 
-def create_all() -> None:
-    os.system("mkdir src && mkdir shared && mkdir core")
-    os.chdir("src")
-    os.system("mkdir modules")
-    os.chdir("modules")
+def create_all(appDirName: str) -> None:
+    make_dir(dirName=appDirName, dirNameSpace=app)
+    os.chdir(f"{appDirName}/modules")
     create_module(name="example")
     os.chdir("..")
+
+
+create_all(appDirName="app")
